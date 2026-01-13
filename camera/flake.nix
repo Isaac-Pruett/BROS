671b434a0da23ff32bbe3796ebe38316cec2fc24
Isaac Pruett <121455801+Isaac-Pruett@users.noreply.github.com>
2026-01-12 19:47:01 -0800

    add otehr camera files.

--- camera/flake.nix
+++ camera/flake.nix
@@ -10,15 +10,43 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        packages.default = naersk-lib.buildPackage {
+
+        gstPluginPath = pkgs.lib.makeSearchPath "lib/gstreamer-1.0" (with pkgs.gst_all_1; [
+          gstreamer
+          gst-plugins-base
+          gst-plugins-good
+          gst-plugins-bad
+          gst-plugins-ugly
+          gst-libav
+        ]);
+
+        unwrappedCamera = naersk-lib.buildPackage {
           src = ./.;
           pname = "camera";
-          # Optional: customize if needed
-          # doCheck = true;
-          # release = false;  # already default in naersk
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+          buildInputs = with pkgs; [
+            gst_all_1.gstreamer
+            gst_all_1.gst-plugins-base
+            gst_all_1.gst-plugins-good
+            gst_all_1.gst-plugins-bad
+            gst_all_1.gst-plugins-ugly
+            gst_all_1.gst-libav
+          ];
         };
+      in
+      {
+        packages.default = pkgs.symlinkJoin {
+          name = "camera";
+          paths = [ unwrappedCamera ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/camera \
+              --set GST_PLUGIN_SYSTEM_PATH_1_0 "${gstPluginPath}"
+          '';
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             cargo
@@ -26,9 +54,18 @@
             rustfmt
             pre-commit
             rustPackages.clippy
+            pkg-config
+            gst_all_1.gstreamer
+            gst_all_1.gst-plugins-base
+            gst_all_1.gst-plugins-good
+            gst_all_1.gst-plugins-bad
+            gst_all_1.gst-plugins-ugly
+            gst_all_1.gst-libav
           ];
           RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+          GST_PLUGIN_SYSTEM_PATH_1_0 = gstPluginPath;
         };
+
         apps.default = utils.lib.mkApp {
           drv = self.packages.${system}.default;
         };
