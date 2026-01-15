4c525c75600c5cc3999a26630ce87ca8900571d0
Isaac Pruett <121455801+Isaac-Pruett@users.noreply.github.com>
2026-01-15 11:53:18 -0800

    update master flake's devshell to have uv again, make the python demo node actually wait on the rust node.
    Signed-off-by: Isaac Pruett <121455801+Isaac-Pruett@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -13,24 +13,7 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-      imports = [
-        # ./module.nix
-        # inputs.foo.flakeModule
-      ];
-
       perSystem = { config, self', inputs', pkgs, system, ... }: let
-        # Allows definition of system-specific attributes
-          # without needing to declare the system explicitly!
-          #
-          # Quick rundown of the provided arguments:
-          # - config is a reference to the full configuration, lazily evaluated
-          # - self' is the outputs as provided here, without system. (self'.packages.default)
-          # - inputs' is the input without needing to specify system (inputs'.foo.packages.bar)
-          # - pkgs is an instance of nixpkgs for your specific system
-          # - system is the system this configuration is for
-
-          # This is equivalent to packages.<system>.default
-
         # Generate shared Zenoh config (customize as needed; could derive from template)
         sharedConfig = pkgs.writeText "zenoh-config.json" ''
           {
@@ -80,8 +63,11 @@
             self'.packages.demo
             self'.packages.rust_demo
             self'.packages.python_demo
+
             self'.packages.my_node
+
             pkgs.just
+            pkgs.uv
           ];
           env.ZENOH_CONFIG = sharedConfig;
           shellHook = ''
@@ -91,10 +77,6 @@
             echo "just is aliased to j"
           '';
         };
-      };
-      flake = {
-        # The usual flake attributes can be defined here, including
-        # system-agnostic and/or arbitrary outputs.
       };
     };
 }
--- python_sub/src/hello_world/__init__.py
+++ python_sub/src/hello_world/__init__.py
@@ -1,16 +1,23 @@
+import sys
 from time import sleep
 
 import zenoh
 
+has_rx = False
+
 
 # a callback to run by the subscriber
 def listen(sample: zenoh.Sample):
+    global has_rx
     print("Python ← Received:", sample.payload.to_string())
+    has_rx = True
 
 
 def main():
+    global has_rx
     with zenoh.open(zenoh.Config().from_env()) as session:
         pub = session.declare_publisher("python/helloworld")
+
         sub = session.declare_subscriber("rust/helloworld", listen)
 
         # Wait for subscribers to be ready
@@ -21,7 +28,8 @@ def main():
         print("Python → Published")
 
         print("Python → Waiting for Rust message...")
-
+        while not has_rx:
+            pass
         print("Python done!")
         session.close()
 
