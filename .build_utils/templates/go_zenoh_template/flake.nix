d2e76a83b7eb21052e95b6f2ec707ea0d90d3355
Isaac Pruett <121455801+Isaac-Pruett@users.noreply.github.com>
2026-06-06 12:49:04 -0700

    add go template

---
+++ .build_utils/templates/go_zenoh_template/flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "go_zenoh_template — Go Zenoh Node";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          zenoh-c = pkgs.callPackage ../.build_utils/zenoh-c.nix { };
+        in
+        {
+          packages.default = pkgs.buildGoModule {
+            pname = "go_zenoh_template";
+            version = "0.1.0";
+            src = ./.;
+            vendorHash = null;
+
+            env.CGO_ENABLED = "1";
+            env.PKG_CONFIG_ALL_STATIC = "1";
+            proxyVendor = true;
+
+            nativeBuildInputs = [
+              pkgs.pkg-config
+            ];
+
+            buildInputs = [
+              zenoh-c
+            ];
+          };
+        };
+    };
+}
